a830905b73b739119b6f2fa45849fd19a4936202
Sumin00 <laurenpsm@naver.com>
2020-12-13 19:54:17 +0900

    Animated AR
    first upload

---
+++ Assets/FoxAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxAnim : MonoBehaviour
+{
+    public Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimLook()
+    {
+        Anim.Play("LookAround", -1, 0f);
+        Anim.speed = 1f;
+    }
+
+    public void AnimWalk()
+    {
+        Anim.Play("Change1", -1, 0f);
+        Anim.speed = 1f;
+    }
+
+    public void AnimRun()
+    {
+        Anim.Play("Change2",-1, 0f);
+        Anim.speed = 1f;
+    }
+    
+}
